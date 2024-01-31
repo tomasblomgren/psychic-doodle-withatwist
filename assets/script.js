5461adc909d0300d2bd0c55f94e66020b6f1e0cf
tomasblomgren <Tomas.blomgren96@gmail.com>
2024-01-31 01:35:36 +0000

    setting up finished task area

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 
-function addtaskcolumn() {
-    document.getElementsByClassName("taskbutton").innerhtml = "taskbutton"
+function addtaskcolumn(addtaskcolumn) {
+    document.getElementsById("taskarea").innerhtml = "taskbutton"
 
 };
 
 function addtaskcolumn() {
     console.log("hellotask")
 }
+console.log("hellotask")
